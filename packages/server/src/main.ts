ff95d339bc5d44a5cca76f52cda399a673deadde
tianyu <tianyu8125@163.com>
2025-04-29 10:42:07 +0800

    feat: update package version

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3100);
-  console.log('Server running at http://127.0.0.1:3100/');
+  const port = process.env.PORT || 3100;
+  await app.listen(port);
+  console.log(`Server running at http://127.0.0.1:${port}/`);
 }
 bootstrap();
